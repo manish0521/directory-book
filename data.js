3b504e17acaea7f2a6bc1df4101d9271895bdcdd
manish singh <manish.singh@codeimmersives.com>
2019-03-27 14:25:31 -0400

    data added

--- data.js
+++ data.js
@@ -1,3 +1,52 @@
-const data = [
-    
+const countries = [
+    {    nation : 'USA',
+         capital : 'DC' ,
+         states : 50,
+         population : '300 millions',
+         attraction : 'Statue of Liberty',
+ 
+     },
+
+     {  nation : 'Nepal',
+        capital : 'Kathmandu' ,
+        states : 7,
+        population : '30 millions',
+        attraction : 'Mount Everest'
+
+    },
+
+     {  nation : 'Japan',
+        capital : 'Tokyo' ,
+        states : 6,
+        population : '126 millions',
+        attraction : 'Mount Fuji'
+
+    },
+
+    {   nation :  'Australia',
+        capital : 'Sydney' ,
+        states : 8,
+        population : '24 millions',
+        attraction : 'Sydney Opera House'
+
+    },
+
+    {   nation :  'India',
+        capital : 'New Delhi' ,
+        states : 22,
+        population : '1034 millions',
+        attraction : 'Taj Mahal'
+
+    },
+
+    {   nation :  'England',
+        capital : 'London' ,
+        states : 0,
+        population : '55 millions',
+        attraction : 'Stonehenge'
+
+    },
+
+
+
 ]
