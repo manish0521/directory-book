321b8b07f119ba3f067b92f20cbe016918159483
manish singh <manish.singh@codeimmersives.com>
2019-03-28 13:04:29 -0400

    search added

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onload = init;
 function init() {
     // When they click the add todo button, run `addTodo`.
     document.querySelector('#search')
-        .addEventListener('click', search);
+        .addEventListener('click', toSearch);
         
         // When they click the clear done todos button, run `clearDoneTodos`.
     document.querySelector('#display-all')
@@ -14,14 +14,34 @@ function init() {
 
 }
 
-function search(event) {
+function toSearch(event) {
     // Stop page from reloading on button click.
     event.preventDefault();
 
+    const search = document.querySelector('#search-input').value;
 
+    for(let i = 0; i < countries.length; i++) {
+      
+        if(search === countries[i].nation) {
+            addToUL(countries[i]);
+        }    
+        if (search === countries[i].capital) {
+        addToUL(countries[i]);
+        }
+        if (search === countries[i].states) {
+            addToUL(countries[i]);
+        }
+        if (search === countries[i].population) {
+            addToUL(countries[i]);
+        }
+        if (search === countries[i].attraction) {
+            addToUL(countries[i]);
+        }
+    }
+}
     
 
-}
+  
 
 function displayAll(event) {
     // Stop page from reloading on button click.
@@ -44,11 +64,18 @@ function addToUL(country) {
     const populationLI = document.createElement('li');
     const attractLI = document.createElement('li');
      
-    nationLI.innerText = country.nation;
-    capitalLI.innerText = country.capital;
-    statesLI.innerText = country.states;
-    populationLI.innerText = country.population;
-    attractLI.innerText = country.attraction;
+    nationLI.innerText = "Country : " + country.nation;
+    capitalLI.innerText = "Capital : " + country.capital;
+    statesLI.innerText = " No. of States : " + country.states;
+    populationLI.innerText = "Population : " + country.population;
+    attractLI.innerText = "Popular Attraction : " + country.attraction;
+
+    capitalLI.className = 'indent' ; 
+    statesLI.className = 'indent' ;
+    populationLI.className = 'indent' ;
+    attractLI.className = 'indent' ;
+
+
 
     
     ul.appendChild(nationLI);
